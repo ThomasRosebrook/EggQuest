f8d1341cd550ddc6d91e57a0af3e71e5e20a8c89
ThomasRosebrook <trosebr@ksu.edu>
2024-11-07 22:30:32 -0600

    Added Basic Player and Input Manager

--- EggQuest/EggQuest/Game1.cs
+++ EggQuest/EggQuest/Game1.cs
@@ -9,6 +9,8 @@ namespace EggQuest
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Egg _theEgg;
+        private Player _player;
+        private InputManager _inputManager;
         private int _screenWidth = 1000;
         private int _screenHeight = 800;
         public Game1()
@@ -24,6 +26,8 @@ namespace EggQuest
             _graphics.PreferredBackBufferHeight = _screenHeight;
             _graphics.ApplyChanges();
 
+            _inputManager = new InputManager();
+            _player = new Player(new Vector2(_screenWidth / 2, _screenHeight / 2));
             _theEgg = new Egg();
             base.Initialize();
         }
@@ -32,14 +36,16 @@ namespace EggQuest
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _theEgg.LoadContent(Content);
+            _player.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            _inputManager.Update(gameTime);
+            if (_inputManager.Exit) Exit();
             _theEgg.Update(gameTime, _screenWidth, _screenHeight);
-
+            _player.Velocity = _inputManager.Direction * 100;
+            _player.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -48,6 +54,7 @@ namespace EggQuest
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
             _theEgg.Draw(gameTime, _spriteBatch);
+            _player.Draw(gameTime, _spriteBatch);
             _spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ EggQuest/EggQuest/Player.cs
@@ -0,0 +1,22 @@
+﻿using EggQuest.Collisions;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EggQuest
+{
+    public class Player : Object2D
+    {
+        public Player (Vector2 position) : base(new BoundingRectangle(position.X, position.Y, 33, 53))
+        {
+            Position = position;
+            Width = 33;
+            Height = 53;
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            Texture = content.Load<Texture2D>("Spatula");
+        }
+    }
+}
