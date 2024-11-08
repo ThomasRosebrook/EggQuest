7e532cca97fa16b28974d6bb15b80c770f9b92b2
Luke <Luke@DESKTOP-41Q6S0V>
2024-11-07 22:34:18 -0600

    added some stuffs

--- EggQuest/EggQuest/Game1.cs
+++ EggQuest/EggQuest/Game1.cs
@@ -11,6 +11,8 @@ namespace EggQuest
         private Egg _theEgg;
         private int _screenWidth = 1000;
         private int _screenHeight = 800;
+        private double timer; 
+        private SpriteFont _font;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -32,6 +34,7 @@ namespace EggQuest
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _theEgg.LoadContent(Content);
+            _font = Content.Load<SpriteFont>("WhateverFontWeWant");//need something here
         }
 
         protected override void Update(GameTime gameTime)
@@ -39,7 +42,7 @@ namespace EggQuest
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             _theEgg.Update(gameTime, _screenWidth, _screenHeight);
-
+            timer += gameTime.ElapsedGameTime.TotalSeconds;
             base.Update(gameTime);
         }
 
@@ -47,7 +50,19 @@ namespace EggQuest
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            _theEgg.Draw(gameTime, _spriteBatch);
+            if(_theEgg.hp <= 0)
+            {/// if you beat the game it will be handled here
+                GraphicsDevice.Clear(Color.Black);
+                Vector2 messageSize = _font.MeasureString("You Won");
+                Vector2 position = new Vector2((_screenWidth - messageSize.X) / 2, (_screenHeight - messageSize.Y) / 2);
+                _spriteBatch.DrawString(_font, "You Won", position, Color.White);
+            }
+            else
+            {
+                _spriteBatch.DrawString(_font, timer.ToString("F2"), new Vector2(50, 50), Color.White);
+                _theEgg.Draw(gameTime, _spriteBatch);
+            }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
